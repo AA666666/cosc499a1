89bc908aedf7ce582a6a17215df721a717264f6f
AA666666 <52806201+AA666666@users.noreply.github.com>
2020-09-15 22:32:39 -0700

    5

--- main.java
+++ main.java
@@ -3,3 +3,28 @@ import java.util.Scanner;
 public class main {
 
 	public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);	
+    
+
+int n1;
+int n2;
+int as;
+int ss;
+
+System.out.println("Enter the first integer number:");
+
+n1=sc.nextInt();
+
+System.out.println("Enter the second integer number:");
+n2=sc.nextInt();
+
+as=n1+n2;
+ss=n1-n2;
+
+
+System.out.println(n1+"+"+n2+"="+as);
+System.out.println(n1+"-"+n2+"="+ss);
+
+}
+//done
+}
